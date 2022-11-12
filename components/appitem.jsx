650486821276966be45ad12478f2d85be0303372
Fábio Machado de Oliveira <machado2@gmail.com>
2022-11-12 01:46:43 -0300

    collection key

--- components/appitem.jsx
+++ components/appitem.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from "next/link";
 
 export default function AppItem({ app }) {
-    const images = app.screenshots.map(url => <Image src={url} className="mr-1 mb-1 w-1/6 inline-block" alt="" width={100} height={0} />)
+    const images = app.screenshots.map(url => <Image key={url} src={url} className="mr-1 mb-1 w-1/6 inline-block" alt="" width={100} height={0} />)
     return <div className="m-4 p-4 bg-green-100 rounded">
         <Link href={'https://play.google.com/store/apps/details?id=' + app.appId} 
             className="font-bold text-lg">{app.title}</Link>
