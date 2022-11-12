b2909040aeda9f1f3f74c0b369c1a7a6fb1f5bdf
Fábio Machado de Oliveira <machado2@gmail.com>
2022-11-12 06:38:38 -0300

    Google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
 import 'antd/dist/antd.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: "G-16GRD2DV99",
+    })
+  }, [])
   return <Component {...pageProps} />
 }
